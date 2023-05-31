51c128ca671797ec44431778c6ef71992c54e2bd
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-06-01 01:42:30 +0530

    1. CartItem(product within Cart): add and remove buttons for  quantity increase decrease

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -20,8 +20,22 @@ const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => {
           <p>Price: ${item.price}</p>
           <p>Total: ${item.amount*item.price}.toFixed(2)</p>
         </div>
-       
+       <div className="buttons">
+        <Button 
+        size="small"
+        disableElevation
+        variant='contained'
+        onClick={()=>removeFromCart(item.id)}
+        />
+        <Button 
+        size="small"
+        disableElevation
+        variant='contained'
+        onClick={()=>addToCart(item)}
+        />
+       </div>
       </div>
+      <img src={item.image} alt={item.title} />
     </Wrapper>
   )
 }
